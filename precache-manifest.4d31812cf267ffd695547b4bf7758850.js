a7b112f1ea37f5ec9e9bc20882813a03561465be
roeljohn <roel@mytractiontools.com>
2019-05-30 21:32:43 +0800

    Updates

---
+++ precache-manifest.4d31812cf267ffd695547b4bf7758850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f623a41b1db7e4c04d784b8205a3947f",
+    "url": "/rj-todo-list-demo/index.html"
+  },
+  {
+    "revision": "9de5ae9aee09249ac833",
+    "url": "/rj-todo-list-demo/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "11fe55d0291f29568a50",
+    "url": "/rj-todo-list-demo/static/css/main.703ef01e.chunk.css"
+  },
+  {
+    "revision": "9de5ae9aee09249ac833",
+    "url": "/rj-todo-list-demo/static/js/2.6dc88e5e.chunk.js"
+  },
+  {
+    "revision": "11fe55d0291f29568a50",
+    "url": "/rj-todo-list-demo/static/js/main.c6c963b0.chunk.js"
+  },
+  {
+    "revision": "4144855e19bb087e3984",
+    "url": "/rj-todo-list-demo/static/js/runtime~main.6ba423eb.js"
+  }
+]);
